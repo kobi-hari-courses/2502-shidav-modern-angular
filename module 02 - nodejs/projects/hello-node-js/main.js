69f7577338f936f61ccf69f0f55989323398def2
Kobi Hari <kobi2294@yahoo.com>
2025-02-05 14:30:04 +0200

    npm fun

---
+++ module 02 - nodejs/projects/hello-node-js/main.js
@@ -0,0 +1,6 @@
+console.log('Hello World');
+var i = 20;
+var j = 30;
+
+console.log(`${i} + ${j} = ${i + j}`);
+
---
+++ module 02 - nodejs/projects/hello-npm/main.js
@@ -0,0 +1,2 @@
+console.log('Hello World');
+console.log('this is a project using npm');
