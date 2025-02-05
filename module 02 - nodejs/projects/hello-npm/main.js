b26457ab55fb4be21d64f271771547330ba5b470
Kobi Hari <kobi2294@yahoo.com>
2025-02-05 15:01:52 +0200

    npm understood

--- module 02 - nodejs/projects/hello-npm/main.js
+++ module 02 - nodejs/projects/hello-npm/main.js
@@ -1,2 +1,9 @@
-console.log('Hello World');
-console.log('this is a project using npm');
+var colors = require('@colors/colors');
+var cheshbon = require('mathjs');
+
+var i = cheshbon.sqrt(-4);
+console.log('the square root of -4 is', i);
+
+
+console.log('Hello World'.green);
+console.log('this is a project using npm'.blue);
