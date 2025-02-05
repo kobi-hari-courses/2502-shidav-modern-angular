f3ad7b8b60d332b9a93c02b43584eca5a1078602
Kobi Hari <kobi2294@yahoo.com>
2025-02-05 15:22:31 +0200

    npm completed

---
+++ module 02 - nodejs/projects/hello-npm/calc.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Calculator = void 0;
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.add = function (a, b) {
+        return a + b;
+    };
+    return Calculator;
+}());
+exports.Calculator = Calculator;
