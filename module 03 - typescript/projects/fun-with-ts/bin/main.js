fcf7d111cb93be27523960d3a7a3233a860357bf
Kobi Hari <kobi2294@yahoo.com>
2025-02-10 12:41:41 +0200

    structure of typescript project

---
+++ module 03 - typescript/projects/fun-with-ts/bin/main.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello World');
+console.log('How are you?');
+var i = 10;
+var j = 20;
+var k = i + j;
+console.log('K = ', k);
---
+++ module 03 - typescript/projects/fun-with-ts/src/main.ts
@@ -0,0 +1,7 @@
+console.log('Hello World');
+console.log('How are you?');
+
+let i = 10;
+let j = 20;
+let k = i + j;
+console.log('K = ', k);
