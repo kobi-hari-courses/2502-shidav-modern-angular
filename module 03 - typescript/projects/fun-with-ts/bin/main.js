978d23189d5d7ef83aa996a45aab2ed448755b4b
Kobi Hari <kobi2294@yahoo.com>
2025-02-10 14:20:29 +0200

    typescript demo completed

--- module 03 - typescript/projects/fun-with-ts/bin/main.js
+++ module 03 - typescript/projects/fun-with-ts/bin/main.js
@@ -5,4 +5,25 @@ var c = (0, calc_1.createCalculator)();
 var i = 10;
 var res = c.add(i, calc_1.PI);
 console.log(res);
+var a = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+for (var _i = 0, a_1 = a; _i < a_1.length; _i++) {
+    var day = a_1[_i];
+    console.log(day);
+}
+var translate = {
+    One: 'Uno',
+    Two: 'Dos',
+    Three: 'Tres',
+    Four: 'Cuatro',
+};
+for (var val in translate) {
+    console.log(val, translate[val]);
+}
+while (i < 20) {
+    console.log(i);
+    i++;
+}
 //# sourceMappingURL=main.js.map
--- module 03 - typescript/projects/fun-with-ts/src/main.ts
+++ module 03 - typescript/projects/fun-with-ts/src/main.ts
@@ -7,3 +7,31 @@ let i = 10;
 let res = c.add(i, PI);
 console.log(res);
 
+const a = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+
+
+for (let day of a) {
+    console.log(day);
+}
+
+const translate: Record<string, string> = {
+    One: 'Uno',
+    Two: 'Dos',
+    Three: 'Tres', 
+    Four: 'Cuatro',
+}
+
+for (let val in translate) {
+    console.log(val, translate[val]);
+}
+
+while (i < 20) {
+    console.log(i);
+    i++;
+}
+
+
