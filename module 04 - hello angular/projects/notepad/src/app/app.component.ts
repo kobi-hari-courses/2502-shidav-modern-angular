aca12136401e7424382b7896908e5cb08ebb2245
Kobi Hari <kobi2294@yahoo.com>
2025-02-12 16:19:32 +0200

    notepad - completed

--- module 04 - hello angular/projects/notepad/src/app/app.component.ts
+++ module 04 - hello angular/projects/notepad/src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  // state
+  readonly colors = ['red', 'green', 'blue', 'purple', 'brown', 'black', 'teal'];
+  readonly fonts = ['Arial', 'Times New Roman', 'Courier New', 'Georgia', 'Verdana', 'Comic Sans MS', 'Impact'];
+  readonly sizes = ['8px', '12px', '16px', '20px', '24px', '28px', '32px'];
+
+  selectedColor = this.colors[0];
+  selectedFont = this.fonts[0];
+  selectedSize = this.sizes[0];
+
+  // actions
+  setSelectedColor(value: string) {
+    console.log('selected color:', value);
+    this.selectedColor = value;
+  }
+
+  setSelectedFont(value: string) {
+    console.log('selected font:', value);
+    this.selectedFont = value;
+  }
+
+  setSelectedSize(value: string) {
+    console.log('selected size:', value);
+    this.selectedSize = value;
+  }
 }
