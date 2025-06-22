e6ee7f1b4983145c6e4b947b6f84cf72d0ea1280
Kobi Hari <kobi2294@yahoo.com>
2025-06-22 11:21:40 +0300

    fun with routing example

--- module 14 - basic routing/projects/fun-with-routing/src/app/app.routes.ts
+++ module 14 - basic routing/projects/fun-with-routing/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PageA } from './pages/page-a/page-a';
+import { PageB } from './pages/page-b/page-b';
+import { PageC } from './pages/page-c/page-c';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'a', pathMatch: 'full'},
+    { path: 'a', component: PageA}, 
+    { path: 'b', component: PageB}, 
+    { path: 'c', component: PageC},
+    { path: '**', component: NotFound}
+];
--- module 14 - basic routing/projects/fun-with-routing/src/app/app.ts
+++ module 14 - basic routing/projects/fun-with-routing/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
