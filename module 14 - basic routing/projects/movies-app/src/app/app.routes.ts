240a9f55c33d2a683a8b102d6e4a9f3446d73864
Kobi Hari <kobi2294@yahoo.com>
2025-06-22 12:15:32 +0300

    now routing through code

--- module 14 - basic routing/projects/movies-app/src/app/app.routes.ts
+++ module 14 - basic routing/projects/movies-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { MoviesList } from './pages/movies-list/movies-list';
+import { MovieDetails } from './pages/movie-details/movie-details';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: Home}, 
-    {path: 'movies', component: MoviesList}
+    {path: 'movies', component: MoviesList}, 
+    {path: 'movies/:id', component: MovieDetails}
 ];
--- module 14 - basic routing/projects/movies-app/src/app/pages/movies-list/movies-list.ts
+++ module 14 - basic routing/projects/movies-app/src/app/pages/movies-list/movies-list.ts
@@ -1,17 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './movies-list.html',
   styleUrl: './movies-list.scss'
 })
 export class MoviesList {
   readonly data = inject(DataService);
+  readonly router = inject(Router);
+
   readonly movies = toSignal(this.data.getAllMovies(), {
     initialValue: []
   });
+
+  goToMovieDetails(movieId: number) {
+    this.router.navigate(['/movies', movieId]);
+  }
 
 }
